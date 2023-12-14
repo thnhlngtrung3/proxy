330d580c804cafceedd86ff95158a10f89756008
thnhlngtrung3 <153743600+thnhlngtrung3@users.noreply.github.com>
2023-12-14 13:47:02 +0700

    Create create_squid_config.sh

---
+++ create_squid_config.sh
@@ -0,0 +1,40 @@
+input_file="check.txt"
+output_file="squid_config_generated.txt"
+num_lines=$(grep -c "inet6.*\/64" "$input_file")
+if [ "$num_lines" -eq 0 ]; then
+  echo "Không có dòng nào chứa \"inet6\" và \"/64\" trong file \"$input_file\"."
+  exit 1
+fi
+
+FIRST_PORT=3128
+echo "" > "$output_file"
+current_port=$FIRST_PORT
+
+http_ports=""
+acl_ports=""
+tcp_outgoing_addresses=""
+
+while IFS= read -r line; do
+  if [[ $line =~ inet6.*\/64 ]]; then
+    ipv6_address=$(echo "$line" | awk '{print $2}' | sed 's:/.*::')
+
+    http_ports+="http_port $current_port, "
+    acl_ports+="acl ip$((current_port - FIRST_PORT + 1)) localport $current_port, "
+    tcp_outgoing_addresses+="tcp_outgoing_address $ipv6_address ip$((current_port - FIRST_PORT + 1)), "
+
+    current_port=$((current_port + 1))
+  fi
+done < "$input_file"
+
+# Remove trailing commas and spaces
+http_ports=${http_ports%, }
+acl_ports=${acl_ports%, }
+tcp_outgoing_addresses=${tcp_outgoing_addresses%, }
+
+# Write the configurations to the output file
+echo "$http_ports" >> "$output_file"
+echo "$acl_ports" >> "$output_file"
+echo "$tcp_outgoing_addresses" >> "$output_file"
+echo "" >> "$output_file"
+
+echo "Đã tạo ra $num_lines cấu hình port trong file \"$output_file\"."
